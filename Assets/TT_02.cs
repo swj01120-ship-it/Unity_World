d6196f38b909a16dc6c70c62a98d6069cedbb011
a01072806941-cpu <a01072806941@gmail.com>
2025-10-23 12:47:40 +0900

    Update TT_02.cs

--- Assets/TT_02.cs
+++ Assets/TT_02.cs
@@ -7,7 +7,7 @@ public class TT_02 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("capsule_tt02")
+        Debug.Log("capsule_tt02");
     }
 
     // Update is called once per frame
