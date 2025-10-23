e512dd023a5f3016d713df6310ab4287c4d98601
a01072806941-cpu <a01072806941@gmail.com>
2025-10-23 11:56:59 +0900

    TT_SQUARE 저장

---
+++ Assets/TT.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class TT : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("TT_SQUARE");
+        Debug.Log("branch");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
